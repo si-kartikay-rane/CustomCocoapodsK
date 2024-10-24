3e7d40026931047f44db53d555b17de49baaaa7e
Kartikay Rane <kartikay.rane@sportzinteractive.net>
2024-10-24 18:03:54 +0530

    Fix

--- DemoTheme/Classes/Themes.swift
+++ DemoTheme/Classes/Themes.swift
@@ -14,18 +14,6 @@ public struct LightTheme: Theme {
     public var textColor: UIColor = .black
     public var buttonColor: UIColor = .blue
     public var font: UIFont = .systemFont(ofSize: 14)
-    
-//    public init(
-//           backgroundColor: UIColor = .white,
-//           textColor: UIColor = .black,
-//           buttonColor: UIColor = .blue,
-//           font: UIFont = .systemFont(ofSize: 14)
-//       ) {
-//           self.backgroundColor = backgroundColor
-//           self.textColor = textColor
-//           self.buttonColor = buttonColor
-//           self.font = font
-//       }
 }
 
 public struct DarkTheme: Theme {
@@ -35,17 +23,4 @@ public struct DarkTheme: Theme {
     public var textColor: UIColor = .white
     public var buttonColor: UIColor = .red
     public var font: UIFont = .systemFont(ofSize: 14)
-        
-//    public init(
-//           backgroundColor: UIColor = .black,
-//           textColor: UIColor = .white,
-//           buttonColor: UIColor = .red,
-//           font: UIFont = .systemFont(ofSize: 14)
-//       ) {
-//           self.backgroundColor = backgroundColor
-//           self.textColor = textColor
-//           self.buttonColor = buttonColor
-//           self.font = font
-//       }
-
 }
