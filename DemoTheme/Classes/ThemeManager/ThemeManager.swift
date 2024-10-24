690d58ea9e5eeb77878af6e5b2daf650847be8e5
Kartikay Rane <kartikay.rane@sportzinteractive.net>
2024-10-24 14:45:03 +0530

    Adding assets and modify access modifiers and making ThemeManager as Observable Object

--- DemoTheme/Classes/ThemeManager/ThemeManager.swift
+++ DemoTheme/Classes/ThemeManager/ThemeManager.swift
@@ -5,30 +5,20 @@
 //  Created by Kartikay Rane on 23/10/24.
 //
 
-public class ThemeManager {
-    // Shared instance accessible globally
+import Foundation
+
+public class ThemeManager: ObservableObject{
     public static let shared = ThemeManager()
 
-    // Private initializer to prevent direct initialization
-    private init() {}
+    public init() {}
 
-    // Public property to access the current theme
-    private var currentTheme: Theme = LightTheme()
+    public var currentTheme: Theme = LightTheme()
 
-    // Method to apply a theme
     public func applyTheme(_ theme: Theme) {
         currentTheme = theme
-        // Notify observers about the theme change
-        NotificationCenter.default.post(name: .themeDidChange, object: nil)
     }
 
-    // Method to get the current theme
     public func getTheme() -> Theme {
         return currentTheme
     }
-}
-
-// Notification for theme change
-extension Notification.Name {
-    public static let themeDidChange = Notification.Name("themeDidChange")
 }
--- DemoTheme/Classes/Themes.swift
+++ DemoTheme/Classes/Themes.swift
@@ -8,39 +8,44 @@
 import Foundation
 
 public struct LightTheme: Theme {
+    public init() {}
+    
     public var backgroundColor: UIColor = .white
     public var textColor: UIColor = .black
     public var buttonColor: UIColor = .blue
     public var font: UIFont = .systemFont(ofSize: 14)
     
-    public init(
-           backgroundColor: UIColor = .white,
-           textColor: UIColor = .black,
-           buttonColor: UIColor = .blue,
-           font: UIFont = .systemFont(ofSize: 14)
-       ) {
-           self.backgroundColor = backgroundColor
-           self.textColor = textColor
-           self.buttonColor = buttonColor
-           self.font = font
-       }
+//    public init(
+//           backgroundColor: UIColor = .white,
+//           textColor: UIColor = .black,
+//           buttonColor: UIColor = .blue,
+//           font: UIFont = .systemFont(ofSize: 14)
+//       ) {
+//           self.backgroundColor = backgroundColor
+//           self.textColor = textColor
+//           self.buttonColor = buttonColor
+//           self.font = font
+//       }
 }
 
 public struct DarkTheme: Theme {
+    public init() {}
+    
     public var backgroundColor: UIColor = .black
     public var textColor: UIColor = .white
     public var buttonColor: UIColor = .red
     public var font: UIFont = .systemFont(ofSize: 14)
         
-    public init(
-           backgroundColor: UIColor = .black,
-           textColor: UIColor = .white,
-           buttonColor: UIColor = .red,
-           font: UIFont = .systemFont(ofSize: 14)
-       ) {
-           self.backgroundColor = backgroundColor
-           self.textColor = textColor
-           self.buttonColor = buttonColor
-           self.font = font
-       }
+//    public init(
+//           backgroundColor: UIColor = .black,
+//           textColor: UIColor = .white,
+//           buttonColor: UIColor = .red,
+//           font: UIFont = .systemFont(ofSize: 14)
+//       ) {
+//           self.backgroundColor = backgroundColor
+//           self.textColor = textColor
+//           self.buttonColor = buttonColor
+//           self.font = font
+//       }
+
 }
